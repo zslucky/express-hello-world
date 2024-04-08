d047f0e6ef5c6c74f8658d0c5bd408d94d1ef54e
Lucky Zhou <zsney.lzhou@gmail.com>
2024-04-08 18:05:45 +0800

    feat: add api

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
+app.post("/api/cb", (req, res) => {
+  res.send('you posted!');
+});
+
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
